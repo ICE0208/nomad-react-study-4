35ce9fb5c2582db8772f7041d327a5e65107182e
ICE0208 <doky3307@naver.com>
2024-02-24 10:42:40 +0900

    refactor: 스타일 컴포넌트 분리

--- react/marvelous-react/src/components/HeroCards.jsx
+++ react/marvelous-react/src/components/HeroCards.jsx
@@ -1,62 +1,20 @@
-import styled from "styled-components";
 import PropTypes from "prop-types";
+import * as S from "../styles/herocards.styled";
 
 export default function HeroCards({ heros = [] }) {
   return (
-    <Wrapper>
+    <S.Wrapper>
       {heros.map((hero) => (
-        <Card key={hero.id}>
-          <CardImg $imgUrl={`${hero.thumbnail.path}.jpg`}>
-            <CardText>{hero.name.split("(")[0].trim()}</CardText>
-          </CardImg>
-        </Card>
+        <S.Card key={hero.id}>
+          <S.CardImg $imgUrl={`${hero.thumbnail.path}.jpg`}>
+            <S.CardText>{hero.name.split("(")[0].trim()}</S.CardText>
+          </S.CardImg>
+        </S.Card>
       ))}
-    </Wrapper>
+    </S.Wrapper>
   );
 }
 
 HeroCards.propTypes = {
   heros: PropTypes.array,
 };
-
-const Wrapper = styled.div`
-  width: 100%;
-  min-height: 100vh;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-  grid-row-gap: 60px;
-  padding: 60px 0;
-  max-width: 1400px;
-`;
-
-const Card = styled.div`
-  background-color: white;
-  justify-self: center;
-  padding: 8px;
-  width: 290px;
-  border-radius: 6px;
-`;
-const CardImg = styled.div`
-  width: 100%;
-  height: 400px;
-
-  background-image: url(${(props) => props.$imgUrl});
-  background-size: cover;
-  background-position: center;
-  border-radius: 6px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  border: 2px solid black;
-`;
-const CardText = styled.p`
-  text-align: center;
-  background-color: white;
-  font-size: 18px;
-  color: black;
-  padding-top: 14px;
-  padding-bottom: 10px;
-  font-weight: bold;
-  border-top: 2px solid black;
-  opacity: 0.9;
-`;
---
+++ react/marvelous-react/src/styles/herocards.styled.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+  grid-row-gap: 60px;
+  padding: 60px 0;
+  max-width: 1400px;
+`;
+
+export const Card = styled.div`
+  background-color: white;
+  justify-self: center;
+  padding: 8px;
+  width: 290px;
+  border-radius: 6px;
+`;
+export const CardImg = styled.div`
+  width: 100%;
+  height: 400px;
+
+  background-image: url(${(props) => props.$imgUrl});
+  background-size: cover;
+  background-position: center;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  border: 2px solid black;
+`;
+export const CardText = styled.p`
+  text-align: center;
+  background-color: white;
+  font-size: 18px;
+  color: black;
+  padding-top: 14px;
+  padding-bottom: 10px;
+  font-weight: bold;
+  border-top: 2px solid black;
+  opacity: 0.9;
+`;

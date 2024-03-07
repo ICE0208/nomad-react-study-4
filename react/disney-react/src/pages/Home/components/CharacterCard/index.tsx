2786bc625d3ec1210fc6d0fd3279d2ffa8d55a51
ICE0208 <doky3307@naver.com>
2024-03-07 23:22:17 +0900

    feat: Not Found 이미지와 같은 크기를 갖는 캐릭터는 숨기기

--- react/disney-react/src/pages/Home/components/CharacterCard/index.tsx
+++ react/disney-react/src/pages/Home/components/CharacterCard/index.tsx
@@ -2,21 +2,46 @@ interface CharacterCardProps {
   imgUrl: string;
   name: string;
 }
+import { useState } from "react";
 
 export default function CharacterCard({ imgUrl, name }: CharacterCardProps) {
+  const [imageError, setImageError] = useState(false);
+
+  const handleImageLoaded = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>,
+  ) => {
+    const imgElement = e.target as HTMLImageElement;
+    const { naturalWidth, naturalHeight } = imgElement;
+
+    // Not Found 응답 이미지의 너비가 200, 높이가 114 이다.
+    if (naturalWidth == 200 && naturalHeight == 114) {
+      setImageError(true);
+    }
+  };
+
   return (
-    <div className="flex h-[280px] w-[360px] flex-col items-center justify-start rounded-3xl bg-gradient-to-b from-violet-700 to-indigo-700 p-6 shadow-2xl ">
-      {/* Image */}
-      <div
-        style={{ backgroundImage: `url("${imgUrl}")` }}
-        className="mb-4 min-h-[180px] w-full rounded-lg bg-white bg-cover bg-top"
-      ></div>
-      {/* Text */}
-      <div className="w-full">
-        <span className="text-2xl font-semibold text-white drop-shadow-xl">
-          {name}
-        </span>
+    !imageError && (
+      <div className="flex h-[280px] w-[360px] flex-col items-center justify-start rounded-3xl bg-gradient-to-r from-violet-700 to-indigo-700 p-6 shadow-2xl ">
+        <img
+          src={imgUrl}
+          alt={name}
+          onLoad={handleImageLoaded}
+          style={{ display: "none" }}
+        />
+
+        {!imageError && (
+          <div
+            className="mb-3 min-h-[170px] w-full rounded-lg bg-white bg-cover bg-center"
+            style={{ backgroundImage: `url(${imgUrl})` }}
+          ></div>
+        )}
+        {/* Text */}
+        <div className="w-full">
+          <span className="text-2xl font-semibold text-white drop-shadow-xl">
+            {name}
+          </span>
+        </div>
       </div>
-    </div>
+    )
   );
 }

fdf55803df4a807b4d44d9debb5d94ace9be3b58
ICE0208 <doky3307@naver.com>
2024-03-07 20:29:58 +0900

    chore: tailwind class sorter 세팅

--- react/disney-react/src/layouts/Navbar/index.tsx
+++ react/disney-react/src/layouts/Navbar/index.tsx
@@ -9,7 +9,7 @@ const getTitle = (pathname: string) => {
 
   // 현재 경로와 매칭되는 패턴 찾기
   const matchingPattern = routePatterns.find((pattern) =>
-    pattern.pattern.test(pathname)
+    pattern.pattern.test(pathname),
   );
   return matchingPattern ? matchingPattern.title : "Page Not Found";
 };
@@ -31,17 +31,14 @@ export default function Navbar() {
 
   return (
     <header>
-      <nav className="h-16 bg-rose-400 text-white flex items-center justify-between px-4 sm:justify-center relative">
-        <div className="text-3xl font-bold flex items-center">
-          <Link
-            to="/"
-            className="mr-1"
-          >
+      <nav className="relative flex h-16 items-center justify-between bg-rose-400 px-4 text-white sm:justify-center">
+        <div className="flex items-center text-3xl font-bold">
+          <Link to="/" className="mr-1">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              className="w-8 h-8 sm:hidden"
+              className="h-8 w-8 sm:hidden"
             >
               <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
               <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
@@ -50,7 +47,7 @@ export default function Navbar() {
           <span className="sm:text-4xl">{title}</span>
           {location.pathname !== "/" && (
             <div
-              className="sm:block hidden absolute left-2 top-1/2 -translate-y-1/2"
+              className="absolute left-2 top-1/2 hidden -translate-y-1/2 sm:block"
               onClick={onBackIconClick}
             >
               <svg
@@ -59,7 +56,7 @@ export default function Navbar() {
                 viewBox="0 0 24 24"
                 strokeWidth={3.0}
                 stroke="currentColor"
-                className="w-8 h-8"
+                className="h-8 w-8"
               >
                 <path
                   strokeLinecap="round"
@@ -70,13 +67,13 @@ export default function Navbar() {
             </div>
           )}
         </div>
-        <div className="flex space-x-2 items-center sm:hidden">
-          <input className="rounded-lg h-8 w-64 text-black px-4 focus:outline-none focus:ring-red-300 focus:ring-2" />
+        <div className="flex items-center space-x-2 sm:hidden">
+          <input className="h-8 w-64 rounded-lg px-4 text-black focus:outline-none focus:ring-2 focus:ring-red-300" />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-7 h-7"
+            className="h-7 w-7"
           >
             <path
               fillRule="evenodd"

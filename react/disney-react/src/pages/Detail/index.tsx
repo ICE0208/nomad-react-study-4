01b811fdc0d932ed2f48005a0db48dba83ace2c6
ICE0208 <doky3307@naver.com>
2024-03-08 17:16:47 +0900

    feat: 디테일 카드 컴포넌트 완성

--- react/disney-react/src/pages/Detail/index.tsx
+++ react/disney-react/src/pages/Detail/index.tsx
@@ -1,3 +1,43 @@
+import getDetail from "@/services/api/getDetails";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import DetailCard from "./components/DetailCard";
+import { useEffect } from "react";
+
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams() as { id: string };
+
+  // useQuery에는 변환된 숫자 ID를 사용
+  const { data, isLoading, isError, error } = useQuery({
+    queryFn: () => getDetail(+id),
+    queryKey: ["detail", +id], // 쿼리 키를 유니크하게 관리하기 위해 ID 포함
+  });
+
+  let content;
+
+  if (isLoading) {
+    content = (
+      <>
+        <span>Loading</span>
+      </>
+    );
+  }
+
+  if (isError) {
+    content = <h1>Something is wrong. {error.message || ""}</h1>;
+  }
+
+  if (data) {
+    content = <DetailCard {...data} />;
+  }
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="flex min-h-[700px] flex-col items-center px-8 py-12 text-white">
+      {content}
+    </div>
+  );
 }

fdad1bfba6ace68b8d7388d3af68eec072cc6c38
ICE0208 <doky3307@naver.com>
2024-03-12 01:34:49 +0900

    styles: 아이템의 스타일 변경

--- react/recoil+forms/src/components/Item/index.tsx
+++ react/recoil+forms/src/components/Item/index.tsx
@@ -17,13 +17,44 @@ export default function ItemComponent({ text, type }: ItemProps) {
   const { handleFirstBtn, handleSecondBtn } = useHandleItemBtn(type, text);
 
   return (
-    <div>
-      <span>{text}</span>
-      <button onClick={handleFirstBtn}>{getFirstBtnText(type)}</button>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: 10,
+        padding: "4px 0",
+      }}
+    >
+      {/* item 텍스트 */}
+      <span style={{ fontSize: 18 }}>{text}</span>
+      {/* 첫 번째 버튼 */}
+      <button
+        onClick={handleFirstBtn}
+        style={buttonStyles}
+      >
+        {getFirstBtnText(type)}
+      </button>
+      {/* 두 번째 버튼 */}
       {/* `좋아하는` 타입의 데이터는 두 번째 버튼을 보여주지않습니다. */}
       {type !== ItemType.좋아하는 && (
-        <button onClick={handleSecondBtn}>{getSecondBtnText(type)}</button>
+        <button
+          onClick={handleSecondBtn}
+          style={buttonStyles}
+        >
+          {getSecondBtnText(type)}
+        </button>
       )}
     </div>
   );
 }
+
+const buttonStyles = {
+  width: "40px",
+  height: "40px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: 12,
+  margin: 0,
+  padding: 0,
+};

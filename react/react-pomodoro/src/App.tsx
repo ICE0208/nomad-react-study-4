48ca762943cefcb5fbdd500464029951d1fe9a1a
ICE0208 <doky3307@naver.com>
2024-03-14 15:38:03 +0900

    feat: Create initial web design

--- react/react-pomodoro/src/App.tsx
+++ react/react-pomodoro/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-full flex-col items-center justify-between p-2">
+      {/* Title */}
+      <div className="flex grow-[2] items-center text-[32px] font-bold">
+        <span>Pomodoro</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      {/* Time */}
+      <div className="flex grow-[4] items-center justify-center gap-1 text-[64px]">
+        <span className="rounded-2xl bg-white px-4 py-6 text-[tomato]">25</span>
+        <span>:</span>
+        <span className="rounded-2xl bg-white px-4 py-6 text-[tomato]">00</span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      {/* Button */}
+      <div className="flex grow-[3] items-center justify-center">
+        <span className="rounded-full bg-[#00000043] p-4">
+          <PlaySVG />
+        </span>
+      </div>
+      {/* Status */}
+      <div className="flex w-full grow-[2] items-center justify-center gap-14">
+        {/* Left Status */}
+        <div className="flex w-[66px] flex-col items-center font-semibold">
+          <span className="font-light">0/4</span>
+          <span>ROUND</span>
+        </div>
+        {/* Right Status */}
+        <div className="flex w-[66px] flex-col items-center font-semibold">
+          <span className="font-light">0/12</span>
+          <span>GOAL</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+const PlaySVG = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className="h-8 w-8"
+  >
+    <path
+      fillRule="evenodd"
+      d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);

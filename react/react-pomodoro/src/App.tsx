238935536273d14bd564d8659aa39a007f7512d6
ICE0208 <doky3307@naver.com>
2024-03-15 00:42:36 +0900

    feat: Add animation to buttons

--- react/react-pomodoro/src/App.tsx
+++ react/react-pomodoro/src/App.tsx
@@ -3,6 +3,7 @@ import { useTimer } from "./hooks";
 import { roundGoalState } from "./recoils";
 import { useMemo } from "react";
 import { MAX_GOAL, MAX_ROUND } from "./constants";
+import { TogglePlayButton } from "./components";
 
 function App() {
   const { remainingMin, remainingSec, toggleTimer, isPlayingTimer } =
@@ -27,14 +28,7 @@ function App() {
         </span>
       </div>
       {/* Button */}
-      <div
-        className="flex grow-[3] items-center justify-center"
-        onClick={toggleTimer}
-      >
-        <span className="rounded-full bg-[#00000043] p-4">
-          {isPlayingTimer ? <PauseSVG /> : <PlaySVG />}
-        </span>
-      </div>
+      <TogglePlayButton onClick={toggleTimer} isPlayingTimer={isPlayingTimer} />
       {/* Status */}
       <div className="flex w-full grow-[2] items-center justify-center gap-14">
         {/* Left Status */}
@@ -57,33 +51,3 @@ function App() {
 }
 
 export default App;
-
-const PlaySVG = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="h-8 w-8"
-  >
-    <path
-      fillRule="evenodd"
-      d="M4.5 5.653c0-1.427 1.529-2.33 2.779-1.643l11.54 6.347c1.295.712 1.295 2.573 0 3.286L7.28 19.99c-1.25.687-2.779-.217-2.779-1.643V5.653Z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
-
-const PauseSVG = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="currentColor"
-    className="h-8 w-8"
-  >
-    <path
-      fillRule="evenodd"
-      d="M6.75 5.25a.75.75 0 0 1 .75-.75H9a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H7.5a.75.75 0 0 1-.75-.75V5.25Zm7.5 0A.75.75 0 0 1 15 4.5h1.5a.75.75 0 0 1 .75.75v13.5a.75.75 0 0 1-.75.75H15a.75.75 0 0 1-.75-.75V5.25Z"
-      clipRule="evenodd"
-    />
-  </svg>
-);
---
+++ react/react-pomodoro/src/components/index.tsx
@@ -0,0 +1,1 @@
+export { default as TogglePlayButton } from "./Button";

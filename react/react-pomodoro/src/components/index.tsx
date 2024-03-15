6d5f508d897a14374c3553eee6126eb0aea7d302
ICE0208 <doky3307@naver.com>
2024-03-15 16:52:42 +0900

    chore: Rename a file

--- react/react-pomodoro/src/components/index.tsx
+++ react/react-pomodoro/src/components/index.tsx
@@ -1,4 +1,4 @@
-export { default as TogglePlayButton } from "./Button";
+export { default as TogglePlayButton } from "./TogglePlayButton";
 export { default as TimeCard } from "./TimeCard";
 export { default as Color } from "./Colon";
 export { default as ResetButton } from "./ResetButton";

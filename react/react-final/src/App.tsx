baad7bead2998634334c83806fb4fa8fe2630cdc
ICE0208 <doky3307@naver.com>
2024-03-18 17:51:38 +0900

    chore: Apply prettier-plugin-tailwindcss

--- react/react-final/src/App.tsx
+++ react/react-final/src/App.tsx
@@ -2,7 +2,8 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-red-500 font-bold text-2xl">
+        {/* prettier-plugin-tailwindcss 으로 인해 클래스의 위치가 변경되었습니다. */}
+        <h1 className="text-2xl font-bold text-red-500">
           테일윈드가 적용되었다면 이 텍스트가 빨간색으로 보여야합니다.
         </h1>
       </div>

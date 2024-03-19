4830eb74e2c45031c2b7eb0cf8921a2792b2293c
ICE0208 <doky3307@naver.com>
2024-03-19 13:19:36 +0900

    feat: Make carousel responsive

--- react/react-final/src/pages/Home/components/SliderArea.tsx
+++ react/react-final/src/pages/Home/components/SliderArea.tsx
@@ -1,25 +1,58 @@
 import { IMovie, makeImagePath } from "@/api";
 import { AnimatePresence, Variants, motion } from "framer-motion";
-import { useMemo, useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
+import { useCardPerSlide } from "../hooks";
 
 interface SliderProps {
   title: string;
   datas: IMovie[];
 }
 
+const cardPerSlide2WidthPerCard = {
+  5: "w-1/6",
+  4: "w-[20%]",
+  3: "w-[25%]",
+  2: "w-[33%]",
+  1: "w-[50%]",
+};
+
+const cardPerSlide2Aspect = {
+  5: "aspect-[7/1]",
+  4: "aspect-[6/1]",
+  3: "aspect-[5/1]",
+  2: "aspect-[4/1]",
+  1: "aspect-[3/1]",
+};
+
 const ANIMATION_DURATION = 0.6;
 
 export default function SliderArea({ title, datas }: SliderProps) {
   const [start, setStart] = useState(0);
   const [wasNext, setWasNext] = useState(true);
-  const isButtonDelay = useRef(true);
+  const isButtonDelay = useRef(false);
+
+  const cardPerSlide = useCardPerSlide();
+
+  const isNeedOneSpace = useMemo(() => {
+    const n = datas.length + cardPerSlide;
+    return n % 2 == 0 ? false : true;
+  }, [datas, cardPerSlide]);
+
+  const clearButtonDelay = useCallback(
+    () => (isButtonDelay.current = false),
+    [],
+  );
 
   const handlePrev = () => {
     if (isButtonDelay.current) return;
     isButtonDelay.current = true;
 
     setWasNext(false);
-    setStart((prev) => (prev - 5) % datas.length);
+    setStart((prev) => {
+      return (prev - cardPerSlide - 1) % datas.length;
+    });
+
+    setTimeout(clearButtonDelay, ANIMATION_DURATION * 1000 + 100);
   };
 
   const handleNext = () => {
@@ -27,10 +60,10 @@ export default function SliderArea({ title, datas }: SliderProps) {
     isButtonDelay.current = true;
 
     setWasNext(true);
-    setStart((prev) => (prev + 5) % datas.length);
-  };
+    setStart((prev) => (prev + cardPerSlide + 1) % datas.length);
 
-  const clearButtonDelay = () => (isButtonDelay.current = false);
+    setTimeout(clearButtonDelay, ANIMATION_DURATION * 1000 + 100);
+  };
 
   const myDatas = useMemo(
     () => [...datas.slice(start), ...datas.slice(0, start)],
@@ -55,33 +88,46 @@ export default function SliderArea({ title, datas }: SliderProps) {
   return (
     <div>
       <h3 className="mb-12">{title}</h3>
-      <AnimatePresence initial={false} custom={wasNext}>
-        <motion.div
-          variants={variants}
-          custom={wasNext}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          onAnimationComplete={clearButtonDelay}
-          transition={{
-            duration: ANIMATION_DURATION,
-            ease: "easeInOut",
-          }}
-          key={start}
-          className="absolute flex w-full flex-nowrap justify-center overflow-x-hidden"
-        >
-          {myDatas.map((data) => {
-            return (
-              <Card
-                key={data.title}
-                title={data.title}
-                backdropPath={data.backdrop_path}
-                className="flex w-[20%] flex-none flex-col items-start px-2"
+      <div className={"bg-red-300 " + cardPerSlide2Aspect[cardPerSlide]}>
+        <AnimatePresence initial={false} custom={wasNext}>
+          <motion.div
+            variants={variants}
+            custom={wasNext}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            // onAnimationComplete={clearButtonDelay}
+            transition={{
+              duration: ANIMATION_DURATION,
+              ease: "easeInOut",
+            }}
+            key={start}
+            className="absolute flex w-full flex-nowrap justify-center overflow-x-hidden"
+          >
+            {myDatas.map((data) => {
+              return (
+                <Card
+                  key={data.title}
+                  title={data.title}
+                  backdropPath={data.backdrop_path}
+                  className={
+                    "flex flex-none flex-col items-start px-2 " +
+                    cardPerSlide2WidthPerCard[cardPerSlide]
+                  }
+                />
+              );
+            })}
+            {isNeedOneSpace && (
+              <div
+                key="space"
+                className={
+                  "flex-none " + cardPerSlide2WidthPerCard[cardPerSlide]
+                }
               />
-            );
-          })}
-        </motion.div>
-      </AnimatePresence>
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </div>
       <div className="mt-[300px]">
         <button onClick={handlePrev}>prev</button>
         <button onClick={handleNext}>next</button>
@@ -101,6 +147,6 @@ const Card = ({
 }) => (
   <div className={className}>
     <img src={makeImagePath(backdropPath)} alt={title} />
-    <span>{title}</span>
+    <span className="w-full text-ellipsis text-nowrap">{title}</span>
   </div>
 );
---
+++ react/react-final/src/pages/Home/hooks/useCardPerSlide.tsx
@@ -0,0 +1,45 @@
+import { debounce } from "lodash";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+type CardPerSlideTypes = 1 | 2 | 3 | 4 | 5;
+
+export default function useCardPerSlide() {
+  const [cardPerSlide, setCardPerSlide] = useState<CardPerSlideTypes>(4);
+
+  const updateCardPerSlide = useCallback(() => {
+    const {
+      body: { clientWidth },
+    } = document;
+
+    // Configure Breakpoints...
+    if (clientWidth <= 650) {
+      setCardPerSlide(1);
+    } else if (clientWidth <= 900) {
+      setCardPerSlide(2);
+    } else if (clientWidth <= 1200) {
+      setCardPerSlide(3);
+    } else if (clientWidth <= 1600) {
+      setCardPerSlide(4);
+    } else {
+      setCardPerSlide(5);
+    }
+  }, []);
+
+  const debounceWindowSizeChange = useMemo(
+    () => debounce(updateCardPerSlide, 50),
+    [updateCardPerSlide],
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", debounceWindowSizeChange);
+
+    return () => window.removeEventListener("resize", debounceWindowSizeChange);
+  }, [debounceWindowSizeChange]);
+
+  useEffect(() => {
+    // 초기 cardPerSlide 세팅
+    updateCardPerSlide();
+  }, [updateCardPerSlide]);
+
+  return cardPerSlide;
+}

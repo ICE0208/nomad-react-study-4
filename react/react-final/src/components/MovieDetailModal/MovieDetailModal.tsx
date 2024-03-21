9c635ec45c04703d03ef0fa30578b92aab827c52
ICE0208 <doky3307@naver.com>
2024-03-22 00:24:49 +0900

    fix: Resolve modal backdrop bug

--- react/react-final/src/components/MovieDetailModal/MovieDetailModal.tsx
+++ react/react-final/src/components/MovieDetailModal/MovieDetailModal.tsx
@@ -24,21 +24,20 @@ export default function MovieDetailModal() {
   });
 
   return (
-    <div className="fixed top-0 flex h-full w-full items-center justify-center bg-[#0000006f]">
+    <div className="fixed top-0 flex h-full w-full items-center justify-center pt-16">
       <div
-        className="absolute left-0 top-0 -z-10 flex h-full w-full items-center justify-center px-24 pt-16"
+        className="absolute left-0 top-0 -z-10 h-full w-[110vw] bg-[#0000006f]"
         onClick={() => navigate(-1)}
+      ></div>
+      <motion.div
+        layoutId={id}
+        className={[
+          "w-[800px] overflow-hidden rounded-2xl bg-[#000000d8]",
+          "aspect-[12/16] md:aspect-[24/18]",
+        ].join(" ")}
       >
-        <motion.div
-          layoutId={id}
-          className={[
-            "w-[800px] overflow-hidden rounded-2xl bg-[#000000d8]",
-            "aspect-[12/16] md:aspect-[24/18]",
-          ].join(" ")}
-        >
-          {data && <ModalContent data={data} />}
-        </motion.div>
-      </div>
+        {data && <ModalContent data={data} />}
+      </motion.div>
     </div>
   );
 }
